cbbcf90a2867568f55c8370de9b2f3c7c1ac8827
Rena <lixunsam@gmail.com>
2022-12-02 16:20:07 +0800

    Tailwind CSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  // darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    maxWidth: {
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
