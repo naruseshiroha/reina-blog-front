4f4cd98106d4f95a0390ea8d66bcf22546fc4ad4
Rena <lixunsam@gmail.com>
2022-12-02 14:44:08 +0800

    自动引入 Naive UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Pages from 'vite-plugin-pages';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-  // vite-plugin-pages
-  Pages({
-    dirs: "src/views",
-    exclude: ["**/components/*.vue"],
-  }),]
-})
+  plugins: [
+    vue(),
+    // vite-plugin-pages
+    Pages({
+      dirs: 'src/views',
+      exclude: ['**/components/*.vue'],
+    }),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar'],
+        },
+      ],
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+    }),
+  ],
+});
