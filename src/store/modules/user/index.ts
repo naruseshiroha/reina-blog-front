12add3ac5fd8f6d95f4f7a65e7d26078cebadaaa
sayo <lixunsam@163.com>
2022-12-11 17:47:26 +0900

    pinia

---
+++ src/store/modules/user/index.ts
@@ -0,0 +1,60 @@
+import { defineStore } from 'pinia';
+import { getVerifyCode } from '@/api/user';
+
+interface IUserState {
+  nickName: string;
+  avatar: string;
+  verifyCode: string;
+  token: string;
+}
+
+// useStore 可以是 useUser、useCart 之类的任何东西
+// 第一个参数是应用程序中 store 的唯一 id
+export const useStore = defineStore('userStore', {
+  // other options...
+  state: (): IUserState => ({
+    nickName: '',
+    avatar: '',
+    verifyCode: '',
+    token: '',
+  }),
+  getters: {
+    getNickName(): string {
+      return this.nickName;
+    },
+    getAvatar(): string {
+      return this.avatar;
+    },
+    getVerifyCode(): string {
+      return this.verifyCode;
+    },
+    getToken(): string {
+      return this.token;
+    },
+  },
+  actions: {
+    patch(partial: Partial<IUserState>) {
+      this.$patch(partial);
+    },
+    reset() {
+      this.$reset();
+    },
+    setNickName(nickName: string) {
+      this.nickName = nickName;
+    },
+    setAvatar(avatar: string) {
+      this.avatar = avatar;
+    },
+    setVerifyCode(verifyCode: string) {
+      this.verifyCode = verifyCode;
+    },
+    setToken(token: string) {
+      this.token = token;
+    },
+    // api
+    async getVerifyCode(email: string) {
+      const { data: code } = await getVerifyCode(email);
+      if (code) this.setVerifyCode(code);
+    },
+  },
+});
