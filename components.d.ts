7809670d87b17f1e86b0384ea31ce722158562f7
sayo <lixunsam@163.com>
2023-01-11 22:00:34 +0900

    auto

--- components.d.ts
+++ components.d.ts
@@ -7,12 +7,16 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    Comment: typeof import('./src/components/comment/index.vue')['default']
+    copy: typeof import('./src/components/rank copy/index.vue')['default']
+    Login: typeof import('./src/components/login/index.vue')['default']
     MyAside: typeof import('./src/components/my-aside.vue')['default']
     MyAsider: typeof import('./src/components/my-asider.vue')['default']
     MyFooter: typeof import('./src/components/my-footer.vue')['default']
     MyHeader: typeof import('./src/components/my-header.vue')['default']
     MySider: typeof import('./src/components/my-sider.vue')['default']
     NAvatar: typeof import('naive-ui')['NAvatar']
+    NBackTop: typeof import('naive-ui')['NBackTop']
     NButton: typeof import('naive-ui')['NButton']
     NCard: typeof import('naive-ui')['NCard']
     NForm: typeof import('naive-ui')['NForm']
@@ -29,9 +33,21 @@ declare module '@vue/runtime-core' {
     NLayoutFooter: typeof import('naive-ui')['NLayoutFooter']
     NLayoutHeader: typeof import('naive-ui')['NLayoutHeader']
     NLayoutSider: typeof import('naive-ui')['NLayoutSider']
+    NList: typeof import('naive-ui')['NList']
+    NListItem: typeof import('naive-ui')['NListItem']
     NMenu: typeof import('naive-ui')['NMenu']
+    NSpace: typeof import('naive-ui')['NSpace']
+    NTabPane: typeof import('naive-ui')['NTabPane']
+    NTabs: typeof import('naive-ui')['NTabs']
     NTag: typeof import('naive-ui')['NTag']
+    NThing: typeof import('naive-ui')['NThing']
+    NTooltip: typeof import('naive-ui')['NTooltip']
+    Random: typeof import('./src/components/random/index.vue')['default']
+    Rank: typeof import('./src/components/rank/index.vue')['default']
+    Recommend: typeof import('./src/components/recommend/index.vue')['default']
+    Register: typeof import('./src/components/register/index.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    Tag: typeof import('./src/components/tag/index.vue')['default']
   }
 }
