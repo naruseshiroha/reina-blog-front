2bb2546cc4105d256fb0ad92bb8f359f1e68d61c
sayo <lixunsam@163.com>
2023-01-13 00:50:30 +0900

    test handleToBottom

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,46 +1,5 @@
 <template>
   <div>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
     Welcome Content <br>
     <!-- bottom: {{ props.isBottom }} <br> -->
     <!-- top: {{ props.top }} <br> -->
@@ -48,6 +7,7 @@
       已经到顶了哦！
     </span> <br>
     top: {{ top }} <br>
+    <p v-for="v in indexs" :key="v">{{ v }}</p>
     bottom: {{ bottom }} <br>
 
     <span v-if="bottom">
@@ -61,6 +21,18 @@
 const props = defineProps({
   top: Boolean,
   bottom: Boolean,
+})
+
+const indexs = ref(50)
+
+watch(() => props.bottom, async (newVal, oldVal) => {
+  console.log('new val: ' , newVal);
+  console.log('old val: ' , oldVal);
+  if (newVal == true) {
+    const res: number = await new Promise((resolve, reject) => resolve(30))
+    console.log('res', res);
+    indexs.value += res;
+  }
 })
 
 </script>
