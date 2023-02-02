f785de0906f57a48fbe3e5083d5ef7da294e9cec
Rena <lixunsam@gmail.com>
2023-02-02 16:07:25 +0800

    isImage

---
+++ src/util/index.ts
@@ -0,0 +1,18 @@
+
+const MIME = [
+  'image/gif',
+  'image/jpeg',
+  'image/avif',
+  'image/png',
+  // 'image/svg+xml',
+  'image/tiff',
+  'image/vnd.wap.wbmp',
+  'image/webp',
+  // 'image/x-icon',
+  'image/x-jng',
+  'image/x-ms-bmp',
+]
+
+export function isImage(mime: string): boolean {
+  return MIME.includes(mime)
+}
