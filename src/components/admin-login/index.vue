3f3b2d5d9e43874a204cbd7e1365bf08d5cb348d
yasuda sayo <lixunsam@163.com>
2023-03-31 07:27:38 +0800

    admin

---
+++ src/components/admin-login/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="w-2/3">
+        <h2 class="text-center text-xl mb-4">レイナのブログ管理</h2>
+        <n-form ref="formRef" label-width="auto" label-placement="left" :model="form" :rules="rules" size="medium">
+            <n-form-item label="用户名" path="username">
+                <n-input v-model:value="form.userName" placeholder="手机号码/邮箱" clearable />
+            </n-form-item>
+            <n-form-item label="密码" path="password">
+                <n-input v-model:value="form.password" type="password" show-password-on="mousedown" placeholder="密码"
+                    :maxlength="16" clearable />
+            </n-form-item>
+            <n-form-item class="login-btns">
+                <n-button @click="handleLoginBtn">登录</n-button>
+            </n-form-item>
+        </n-form>
+    </div>
+</template>
+
+<script setup lang="ts">
+const formRef = ref()
+const form = reactive({
+    userName: '',
+    password: '',
+})
+const rules = []
+const handleLoginBtn = () => {
+    // check is admin
+    console.log("form", unref(form));
+
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/admin/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="relative h-full">
+        <!-- Admin Page -->
+        <!-- <h2 class="text-2xl text-center font-bold">レイナ　の　ブログ　管理</h2> -->
+        <AdminLogin class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped></style>
