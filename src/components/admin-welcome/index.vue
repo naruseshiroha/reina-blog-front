1689a0dc1ef6b529b46ea74bde0e44c2287d6716
yasuda sayo <lixunsam@163.com>
2023-04-01 23:26:01 +0800

    welcome

---
+++ src/components/admin-welcome/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="h-full relative">
+        <n-image class="absolute top-0 left-0" src="/img/bg/bg01.jpg"></n-image>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss"></style>
