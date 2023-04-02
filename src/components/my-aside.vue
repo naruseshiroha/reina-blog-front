3e76e6ea4165a583983fa905a147df0deb7d616d
yasuda sayo <lixunsam@163.com>
2023-04-02 20:42:36 +0800

    search

--- src/components/my-aside.vue
+++ src/components/my-aside.vue
@@ -2,8 +2,9 @@
   <div class="aside">
     <!-- search -->
     <n-input-group>
-      <n-input maxlength="20" placeholder="请输入关键字进行搜索" show-count clearable />
-      <n-button type="primary" ghost>搜索</n-button>
+      <n-input v-model:value="keyword" @keyup="handleKeyUp" maxlength="20" placeholder="请输入关键字进行搜索" show-count
+        clearable />
+      <n-button type="primary" ghost @click="handleClickSearch">搜索</n-button>
     </n-input-group>
 
     <Login />
@@ -20,6 +21,19 @@
 
 <script setup lang="ts">
 import Login from "/@/views/login/index.vue";
+
+const keyword = ref('')
+const router = useRouter();
+const handleClickSearch = () => {
+  router.replace({ name: "index", query: { key: keyword.value } })
+}
+
+const handleKeyUp = (e) => {
+  if (e.key === 'Enter') {
+    handleClickSearch()
+  }
+}
+
 </script>
 
 <style lang="scss" scoped></style>
